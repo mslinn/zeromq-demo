1f1249932473eabce2c59de52b4d9416250eaa60
Mike Slinn <mslinn@micronauticsresearch.com>
2012-04-20 15:52:05 -0700

    Improved type formatting

--- src/main/scala/com/micronautics/util/ByteFormatter.scala
+++ src/main/scala/com/micronautics/util/ByteFormatter.scala
@@ -5,9 +5,10 @@ object ByteFormatter {
   
   def format(v: Double, precision: Int=2): String = {
     def formatR(n: Double, list: List[String]): String = {
-      if(n < 1024.0 || list.size == 1) {
+      if (n < 1024.0 || list.size == 1) {
         val s = n.toString
-        val t = s.substring(0, math.min(precision, s.length))
+        val dot = s.indexOf(".")
+        val t = s.substring(0, math.min(dot + precision + 1, s.length))
         t + list.head
       } else {
         formatR(n/1024.0, list.tail)
@@ -16,5 +17,5 @@ object ByteFormatter {
     formatR(v, units)
   }
 
-  def format(l: Long): String = format(l.asInstanceOf[Double])
+  def apply(l: Long): String = format(l.asInstanceOf[Double])
 }
